4615d441d278c4ff0fd384d0d95b04e4105a9be3
Mihajlo Milic <mihajlo.milic@elfak.rs>
2019-08-31 23:46:20 +0200

    Scores Added

--- RXJS/src/rx.js
+++ RXJS/src/rx.js
@@ -1,46 +1,42 @@
-import { from, fromEvent, Subject, zip, interval, timer } from "rxjs";
-import { debounceTime, map, switchMap, filter, scan, pairwise, take, timeInterval, takeUntil } from "rxjs/operators";
+import { from, fromEvent, Subject, zip, interval, timer, of, Observable } from "rxjs";
+import { debounceTime, map, switchMap, filter, scan, pairwise, take, timeInterval, takeUntil, merge, mergeMap, delay, takeLast, toArray  } from "rxjs/operators";
 import { ajax } from 'rxjs/ajax';
 
 
 
 
-let interval$ = timer(0, 2000);
+let interval$ = timer(0, 3698);
 let time$ = timer(0, 1000);
-let gameEnd$ = timer(10000)
-let teams$ = getMatches();
+let gameEnd$ = timer(90000)
+let matches$ = getMatches();
 let pulledData$ = new Subject();
 let pull$ = new Subject();
+let teams$ = getTeams();
+teams$.subscribe(el => updateScoreTable(el))
 
 pull$ = time$.pipe(
-    switchMap(() => teams$)
+    switchMap(() => matches$)
 )
 pulledData$ = interval$.pipe(
-    switchMap(() => teams$)
+    switchMap(() => matches$)
 );
-
 
 pull$.pipe(takeUntil(gameEnd$)).subscribe(el => reduceTime(el))
 
 pulledData$.pipe(takeUntil(gameEnd$)).subscribe(res => scoreGoal(res));
+let subscribers$ = new Subject()
+let endedMatches = from(zip(matches$, gameEnd$).pipe(map(el => el[0])))
+endedMatches.subscribe(item => filterScores(subscribers$, item))
+let firstTeamWin = subscribers$.pipe(filter(el => el.GoalsTeam1 > el.GoalsTeam2))
+let secoundTeamWin = subscribers$.pipe(filter(el => el.GoalsTeam2 > el.GoalsTeam1))
+let winners = firstTeamWin.pipe(merge(secoundTeamWin)).subscribe(el => {console.log("Mecevi sa Pobednicima su "); console.log(el)})
+
+zip(endedMatches, teams$).subscribe(el => updateScore(el))
 
 
-// let endedMatches = zip(teams$, gameEnd$).pipe(map(el => el[0]))
-
-// let winersHome = endedMatches.filter(item => item.GoalsTeam1 > item.GoalsTeam2).subscribe(el => console.log(el))
-// let allWiningTeams = teams$.pipe(filter(T => T.GoalsTeam1 > 0))
-// allWiningTeams.subscribe(el => console.log(" dsadas " + el));
-
-function reduceTime(Match) {
-    console.log("Uso sam");
-    Match.forEach(el => {
-        el.Time = el.Time - 1;
-    })
-    updateTable(Match);
-}
 
 function getTeams() {
-    return from(fetch(`http://localhost:3000/Teams`).then(res => res.json()));
+    return from(fetch(`http://localhost:3000/teams`).then(res => res.json()));
 }
 
 function getMatches() {
@@ -56,26 +52,68 @@ function randomNumber() {
 function scoreGoalRandomTeam(el) {
     let rand = Math.random() * 10;
     let number = Math.round(rand);
-    console.log(number);
+
     return number % el;
 }
 
+function reduceTime(Match) {
+
+    Match.forEach(el => {
+        el.Time = el.Time + 1;
+    })
+    updateTable(Match);
+}
+
+function updateScore(match)
+{
+    match[0].forEach(el=>
+        {
+            if (el.GoalsTeam1>el.GoalsTeam2)
+        {
+            match[1].forEach(club =>
+            {
+                if (el.Team1.name === club.name)
+                {
+                    club.Points += 3;
+                }
+            })
+        }
+        else if(el.GoalsTeam2>el.GoalsTeam1)
+        {
+            match[1].forEach(club =>
+                {
+                    if (el.Team2.name === club.name)
+                    {
+                        club.Points += 3;
+                    }
+                })
+        }
+        else{
+            match[1].forEach(club =>
+                {
+                    if (el.Team1.name === club.name || el.Team2.name === club.name)
+                    {
+                        club.Points += 1;
+                    }
+                })
+        }
+        })
+    updateScoreTable(match[1])
+}
 
 function getLength(pulledData$) {
     let number = 0;
     pulledData$.forEach(el => {
         number++;
     })
-    console.log(number);
-    return number - 1;
+
+    return number ;
 }
 
 function scoreGoal(match) {
-    console.log(match);
     let numb = getLength(match);
 
     let index = scoreGoalRandomTeam(numb);
-    console.log(index);
     let number = randomNumber() % 2;
     if (number === 0) {
         match[index].GoalsTeam1 = match[index].GoalsTeam1 + 1;
@@ -86,23 +124,92 @@ function scoreGoal(match) {
     }
 
 }
+function filterScores(subscribers$, item) {
+    item.forEach(el =>
+        {
+            subscribers$.next(el);
+        }
+        )
+}
+
 
 function updateTable(Match) {
+    
     let Tabel = document.getElementById("Tabela");
+    Tabel.style.border = "3px solid #000"
     Tabel.innerHTML = "";
+    let head = document.createElement("thead")
+    head.style.border = "3px solid #000 collapse"
+    let row = document.createElement("tr");
+    let data = document.createElement("th");
+    data.innerHTML = "Team 1";
+    data.style.border = "3px solid #000"
+    row.appendChild(data);
+    data = document.createElement("th");
+    data.innerHTML = "Score";
+    data.style.border = "3px solid #000"
+    row.appendChild(data);
+    data = document.createElement("th");
+    data.innerHTML = "Team 2";
+    data.style.border = "3px solid #000"
+    row.appendChild(data);
+    data = document.createElement("th");
+    data.innerHTML = "Time";
+    data.style.border = "3px solid #000"
+    row.appendChild(data);
+    head.appendChild(row)
+    Tabel.appendChild(head);
     Match.forEach(el => {
         let row = document.createElement("tr");
+        row.style.border = "3px solid #000"
         let data = document.createElement("th");
+        data.style.border = "3px solid #000"
         data.innerHTML = el.Team1.name;
         row.appendChild(data);
         data = document.createElement("th");
         data.innerHTML = el.GoalsTeam1 + " : " + el.GoalsTeam2;
+        data.style.border = "3px solid #000"
         row.appendChild(data);
         data = document.createElement("th");
         data.innerHTML = el.Team2.name;
+        data.style.border = "3px solid #000"
         row.appendChild(data);
         data = document.createElement("th");
         data.innerHTML = el.Time;
+        data.style.border = "3px solid #000"
+        row.appendChild(data);
+        Tabel.appendChild(row);
+    })
+}
+
+function updateScoreTable(match)
+{   
+    let Tabel = document.getElementById("Scores");
+    Tabel.style.border = "3px solid #000"
+    Tabel.innerHTML = "";
+    let head = document.createElement("thead")
+    head.style.border = "3px solid #000 collapse"
+    let row = document.createElement("tr");
+    let data = document.createElement("th");
+    data.innerHTML = "Team Name";
+    data.style.border = "3px solid #000"
+    row.appendChild(data);
+    data = document.createElement("th");
+    data.innerHTML = "Points";
+    data.style.border = "3px solid #000"
+    row.appendChild(data);
+    head.appendChild(row)
+    Tabel.appendChild(head);
+    match.forEach(el=>{
+        let row = document.createElement("tr");
+        row.style.border = "3px solid #000"
+        let data = document.createElement("th");
+        data.style.border = "3px solid #000"
+        data.innerHTML = el.name;
+        row.appendChild(data);
+        data = document.createElement("th");
+        data.innerHTML = el.Points;
+        data.style.border = "3px solid #000"
         row.appendChild(data);
         Tabel.appendChild(row);
     })
